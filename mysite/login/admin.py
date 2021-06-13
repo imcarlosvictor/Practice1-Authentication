b55dd046a138846e948d91becd4ab6ad1e7981f6
Carlos Victor Torres <carlosvictortorres96@gmail.com>
2021-06-12 23:16:48 -0400

    admin compelted

--- mysite/login/admin.py
+++ mysite/login/admin.py
@@ -5,12 +5,31 @@ from .models import Person
 
 
 # Register your models here.
+class EmailFilter(admin.SimpleListFilter):
+    """Custom filter for finding users with no email."""
+
+    title = 'Email Filter'
+    parameter_name = 'email'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('has_email', 'has_email'),
+            ('no_email', 'no_email'),
+        )
+
+    def queryset(self, request, queryset):
+        if not self.value():
+            return queryset
+        if self.value().lower() == 'has_email':
+            return queryset.exclude(email='')
+        if self.value().lower() == 'no_email':
+            return queryset.filter(email='')
+
+
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('id', 'last_name', 'first_name', 'gender', 'email',
                     'date_created')
-    list_filter = ('gender', )
-
-
-class EmailListFilter(SimpleListFilter):
-    pass
+    list_filter = ('gender', EmailFilter)
+    ordering = ('-last_name', )
+    search_fields = ['last_name', 'first_name']
